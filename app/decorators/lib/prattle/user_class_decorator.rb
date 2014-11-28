979e57eb4aec5a610c404b83e345219c51a0852e
Guy Molyneux <guy.molyneux@gmail.com>
2014-11-28 07:16:09 +0000

    Tidied up the decorators

--- app/decorators/lib/prattle/user_class_decorator.rb
+++ app/decorators/lib/prattle/user_class_decorator.rb
@@ -1,3 +1,5 @@
-Prattle.user_class.class_eval do
-	has_many :prattle_topic_read_up_to_marks, :class_name => "Prattle::TopicReadUpToMark", :foreign_key => "user_id"
+if Prattle.user_class
+	Prattle.user_class.class_eval do
+		has_many :prattle_topic_read_up_to_marks, :class_name => "Prattle::TopicReadUpToMark", :foreign_key => "user_id"
+	end
 end
--- lib/prattle.rb
+++ lib/prattle.rb
@@ -6,4 +6,16 @@ require 'decorators'
 
 module Prattle
 	mattr_accessor :user_class, :current_user_method
+
+	class << self
+		def user_class
+	      if @@user_class.is_a?(String)
+	        begin
+	          Object.const_get(@@user_class)
+	        rescue NameError
+	          @@user_class.constantize
+	        end
+	      end
+	    end
+	end
 end
