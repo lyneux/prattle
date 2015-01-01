09d021f397b0effe5474818a677165777dd43144
Guy Molyneux <guy.molyneux@gmail.com>
2015-01-01 08:04:20 +0000

    No longer sends a notification to the post author

--- app/helpers/prattle/notifications_helper.rb
+++ app/helpers/prattle/notifications_helper.rb
@@ -5,9 +5,11 @@ module Prattle
     	puts "Processing notifications for new post: " + @post.id.to_s
     	member_settings_to_notify = Prattle::MemberSettings.where(notify_all: true)
     	member_settings_to_notify.each do |member_settings|
-        puts "Sending email to: " + member_settings.user.email if member_settings.user.email
-        post_notification = Prattle::PostNotifications.new(:post => @post, :user => member_settings.user, :sent => false)
-        post_notification.save
+        unless @post.user == member_settings.user
+          puts "Sending email to: " + member_settings.user.email if member_settings.user.email
+          post_notification = Prattle::PostNotifications.new(:post => @post, :user => member_settings.user, :sent => false)
+          post_notification.save
+        end
       end
     end
     
